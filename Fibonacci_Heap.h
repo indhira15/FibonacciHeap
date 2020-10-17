dde5076f2f1ddf8e1e9f612548b006e1595de246
indhira15 <indhira.ramirez@utec.edu.pe>
2020-10-17 16:34:42 -0500

    maybe done

--- Fibonacci_Heap.h
+++ Fibonacci_Heap.h
@@ -6,6 +6,7 @@
 #include <list>
 #include <limits>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -32,26 +33,168 @@ public:
 		return n;
 	}
 	void InsertNodo(NodoT *n){
-		fb_heap.push_back(n);
+		fb_heap.push_front(n);
 	}
-	/*
+
 	NodoT* GetNewMinNodo(){
-
+		if(fb_heap.size()>0){
+			auto aux = *fb_heap.begin();
+			for(auto &a:fb_heap){
+				if(a->key < aux->key){
+					aux = a;
+				}
+			}
+			return aux;
+		}
+		else
+			return nullptr;
 	}
+
 	NodoT* GetMinNodo(){
-
+		if(fh_size>0)
+			return nodo_min;
+		else
+			return nullptr;
 	}
-	NodoT* GetMinNodoValue(){
-
+	T GetMinNodoValue(){
+		if(fh_size > 0)
+			return nodo_min->key;
+		else
+			return -1;
 	}
 	NodoT* DeleteMin(){
 		// retorna el min y lo borra
+		if(fh_size > 0){
+			NodoT* aux = GetMinNodo();
+			auto itr_child = aux->children.begin();
+			while(itr_child != aux->children.end()){
+				// maybe volvelos los hijos blancos
+				InsertNodo(*itr_child);
+			}
+			fb_heap.remove(aux);
+			nodo_min = GetNewMinNodo();
+
+			Compactar();
+			return aux;
+		}
+		else
+			return nullptr;
 	}
+	// siempre ponerlo en la posicion del primer nodo
+	NodoT* Unir_dos_nodos(NodoT* &a, NodoT* &b){
+		if(a->key < b->key){
+	      a->rank = (max(a->rank, b->rank+1));
+	      b->padre = a;
+	      a->children.push_front(b);
+	      fb_heap.remove(b);
+	      return a;
+	    }
+	    else{
+	      b->rank = (max(b->rank, a->rank+1));
+	      a->padre = b;
+	      b->children.push_front(a);
+	      fb_heap.remove(b);
+	      a = b;
+	      return a;
+	    }
+	}
+
 	void Compactar(){
+		int tm = log2(fh_size) +1;
+		vector<NodoT*> v(tm);
+		fill(v.begin(), v.end(), nullptr);
+		NodoT* nodo_aux;
+		for(auto i=fb_heap.begin(); i!=fb_heap.end(); ++i){
+			if(v[(*i)->rank] != nullptr){
+				int aux2 = (*i)->rank;
+				nodo_aux = Unir_dos_nodos((*i),v[(*i)->rank]);
+				v[aux2] = nullptr;
+				--i;
+			}
+			else{
+				v[(*i)->rank] = (*i);
+			}
+		}
+
 
 	}
-	*/
 
+	void print(){
+		for(auto &a:fb_heap){
+			cout << a->key << "\t";
+		}
+		cout << endl;
+	}
+
+	void PrintSubTree(NodoT* root) {
+	    if (root) { 
+	        for(auto it : root->children){
+	            PrintSubTree(it);
+	            cout << it->key << " ";
+	        }
+	    }
+	}
+
+	void PrintSubTreeSpecial(NodoT* root) {
+	    if (root) { 
+	        for(auto it : root->children){
+	            cout << root->key << " -> ";
+	            cout << it->key << ";\n";
+	            cout << it->key << " -> ";
+	            PrintSubTree(it);
+	        }
+	    }
+	}
+
+	void printSpecial() {
+
+	  for(auto it = fb_heap.begin(); it != fb_heap.end(); it++){
+	    cout << (*it)->key << ";\n";
+	    // cout << "==Begin Printing sub tree==\n";
+	    PrintSubTreeSpecial(*it);
+	    // cout << "\n==End Printing sub tree==\n";
+	}
+
+	}
+	void generatePDF() {
+	  fstream file("FibonacciHeap.vz", fstream::out | fstream::trunc);
+
+	  if (file.is_open()) {
+	    file << "digraph G\n";
+	    file << "{\n";
+
+	    printSpecial(file);
+
+	    file << "}\n";
+	    file.close();
+	    system("dot -Tpdf FibonacciHeap.vz -o FibonacciHeap.pdf");
+	  }
+	}
+
+	void PrintSubTreeSpecial(fstream& file, NodoT* root) {
+	    if (root) { 
+	      file << "\"" << root <<"\" [\n";
+	      file << "\tlabel = "<< root->key <<"\n]\n";
+	        for(auto it : root->children){
+	            if (it->key != root->key) {
+	              file << "\"" << root <<"\" -> ";
+	              file << "\"" << it << "\";\n";
+	              /*
+	              file << root->m_key << " -> ";
+	              file << it->m_key << ";\n";*/
+	              PrintSubTreeSpecial(file, it);
+	            }
+	        }
+	    }
+	}
+
+	void printSpecial(fstream& file) {
+	  for(auto it = fb_heap.begin(); it != fb_heap.end(); it++) {
+	    file << "\"" << (*it) <<"\" [\n";
+	    file << "\tlabel = "<< (*it)->key <<"\n]\n";
+	    PrintSubTreeSpecial(file, *it);
+	  }
+	}
 };
 
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main() {
 
-    Fibonacci_heap<float> fh();
+    Fibonacci_heap<float>* fh = new Fibonacci_heap<float>();
 
+    vector<NodoF<float>*> v;
+    for(int i=3; i<=10; ++i){
+        v.push_back(fh->Insert(i));
+    }
+    v.push_back(fh->Insert(1));
+    fh->print();
+    fh->DeleteMin();
+    fh->print();
+    cout << fh->GetMinNodoValue() << endl;
+    fh->generatePDF();
     return 0;
 }
